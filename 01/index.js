d1d45d39ad7428d821d5ce6cf7112dbd53eee4a1
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-14 19:05:04 +0530

    modules, require and exports

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,7 @@
-console.log("hey from console");
+const math = require("./math.js")
+//const { add, sub} = require("./math.js")
+
+//  ./ means current directory
+//  "xyz" means xyz is a built in feature and searches it in the node directory
+console.log(math.add(2,3));
+console.log(math.sub(2,3));
---
+++ 01/math.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+//export the function
+module.exports = {
+    add,
+    sub,
+}
